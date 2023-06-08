95eab0a3f0f5c4c2eb75a206ea53b3fd96f1a49e
hvu26510 <hvu26510@gmail.com>
2023-06-08 18:54:27 +0700

    Add project files.

---
+++ ExceptionDemo/CheckScoreException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    internal class CheckScoreException : Exception
+    {
+        public CheckScoreException(string msg)
+            : base(msg)
+        {
+        }
+    }
+}
---
+++ ExceptionDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diem = -1;
+            try
+            {
+                diem = NhapDiem();
+            }catch (CheckScoreException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+            #region
+            //try
+            //{
+            //    x = int.Parse(Console.ReadLine());
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //    Console.WriteLine("Nhap lai du lieu");
+            //    x = int.Parse(Console.ReadLine());
+            //}
+            //finally
+            //{
+            //    if (x >= 10)
+            //        Console.WriteLine("x = " + x);
+            //}
+            #endregion
+
+
+            Console.ReadLine();
+        }
+
+
+        static double NhapDiem()
+        {
+            double diem = -1;
+            Console.WriteLine("Nhap diem:");
+            diem = double.Parse(Console.ReadLine());
+            if(diem<0||diem>10)
+            {
+                throw new CheckScoreException("Diem khong hop le");
+            }
+            return diem;
+        }
+    }
+}
